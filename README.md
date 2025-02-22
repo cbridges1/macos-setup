4a1e76a94e377881b55f606e0dab0fd3321828cd
Jalen <cedric.bridges3@gmail.com>
2025-02-22 13:00:32 -0600

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+For setup:
+```
+curl s <repo> | bash -s
+```
+Login to github
+For installs:
+```
+curl -s <repo> | bash -s
+```
---
+++ install.sh
@@ -0,0 +1,4 @@
+mkdir /Users/jalenbridges/source
+cd /Users/jalenbridges/source
+git clone https://github.com/cbridges1/macos-installs.git
+cd macos-installs
