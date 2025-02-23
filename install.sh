c5953719fff24b6fc3a3e8ef9c6abce08268af32
jalen <cedric.bridges3@gmail.com>
2025-02-23 10:34:09 -0600

    init

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 mkdir /Users/jalenbridges/source
 cd /Users/jalenbridges/source
-git clone https://github.com/cbridges1:$1/macos-config.git
+git clone https://cbridges1:$1@github.com/cbridges1/macos-config.git
 cd macos-config
 bash setup.sh
