ffab7d9ce31ab30af90f2fe9d367af4cf58eae77
jalen <cedric.bridges3@gmail.com>
2025-02-23 10:52:40 -0600

    wip

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 For setup:
 ```
-curl s https://raw.githubusercontent.com/cbridges1/macos-setup/refs/heads/main/install.sh | bash -s -- github-token
+curl s https://raw.githubusercontent.com/cbridges1/macos-setup/refs/heads/main/install.sh | bash -s -- user github-token
 ```
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir /Users/jalenbridges/source
-cd /Users/jalenbridges/source
-git clone https://cbridges1:$1@github.com/cbridges1/macos-config.git
+mkdir /Users/$1/source
+cd /Users/$1/source
+git clone https://cbridges1:$2@github.com/cbridges1/macos-config.git
 cd macos-config
 bash setup.sh
