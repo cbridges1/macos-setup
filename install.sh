7cde35dfd5bda8acd88f50b3efa3141d4c971972
jalen <cedric.bridges3@gmail.com>
2025-02-23 10:23:26 -0600

    init

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+
 mkdir /Users/jalenbridges/source
 cd /Users/jalenbridges/source
-git clone https://github.com/cbridges1/macos-installs.git
-cd macos-installs
+git clone https://github.com/cbridges1:$1/macos-config.git
+cd macos-config
+bash setup.sh
--- setup.sh
+++
@@ -1,6 +0,0 @@
-sudo -u jalenbridges defaults write com.apple.finder AppleShowAllFiles TRUE ; killall Finder
-defaults write -g com.apple.swipescrolldirection FALSE
-
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install --cask github
-brew install --cask bitwarden
