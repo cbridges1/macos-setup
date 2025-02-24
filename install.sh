9af4764a07b54e34e3ed319d975917e856dc3078
jalen <cedric.bridges3@gmail.com>
2025-02-24 17:22:36 -0600

    wip

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ cd /opt/homebrew/bin/
 # PATH=$PATH:/opt/homebrew/bin
 echo export PATH=$PATH:/opt/homebrew/bin >> ~/.zshrc
 mkdir ~/.nvm
-echo export NVM_DIR="$HOME/.nvm" >> ~/.zshrc
+echo export PATH=$PATH:/$HOME/.nvm >> ~/.zshrc
 brew install --cask github
 mkdir /Users/$1/source
 cd /Users/$1/source
