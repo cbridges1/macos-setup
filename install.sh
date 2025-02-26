2ee8f204f6b230a827e6b87ab9c1e3dae5f1269f
jalen <cedric.bridges3@gmail.com>
2025-02-25 20:00:20 -0600

    wip

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
-# cd /opt/homebrew/bin/
-# PATH=$PATH:/opt/homebrew/bin
-# echo export PATH=$PATH:/opt/homebrew/bin >> ~/.zshrc
-cd $HOME
-curl https://raw.githubusercontent.com/cbridges1/macos-setup/refs/heads/main/.zshrc > .zshrc
-curl https://raw.githubusercontent.com/cbridges1/macos-setup/refs/heads/main/.zprofile > .zprofile
+cd ~
+curl -O https://raw.githubusercontent.com/cbridges1/macos-setup/refs/heads/main/.zshrc > .zshrc
+curl -O https://raw.githubusercontent.com/cbridges1/macos-setup/refs/heads/main/.zprofile > .zprofile
 mkdir ~/.nvm
-echo export PATH=$PATH:/$HOME/.nvm >> ~/.zshrc
+source .zshrc
+source .zprofile
 brew install --cask github
 mkdir /Users/$1/source
 cd /Users/$1/source
