a4e92205a7fd4ccd10a869bc81a6ac7e7d16042e
jalen <cedric.bridges3@gmail.com>
2025-03-07 08:37:11 -0600

    wip

--- install.sh
+++ install.sh
@@ -1,15 +1,13 @@
 cd ~
-curl -O https://raw.githubusercontent.com/cbridges1/macos-setup/refs/heads/main/.zshrc > .zshrc
-curl -O https://raw.githubusercontent.com/cbridges1/macos-setup/refs/heads/main/.zprofile > .zprofile
-mkdir ~/.nvm
-source .zshrc
-source .zprofile
-mkdir source
-cd source
-# mkdir /Users/$1/source
-# cd /Users/$1/source
-mkdir config
+# curl -O https://raw.githubusercontent.com/cbridges1/macos-setup/refs/heads/main/.zshrc > .zshrc
+# curl -O https://raw.githubusercontent.com/cbridges1/macos-setup/refs/heads/main/.zprofile > .zprofile
+# mkdir ~/.nvm
+# source .zshrc
+# source .zprofile
+# mkdir source
+# cd source
+# mkdir config
 cd config
-git clone https://cbridges1:$1@github.com/cbridges1/macos-config.git
+# git clone https://cbridges1:$1@github.com/cbridges1/macos-config.git
 cd macos-config
 bash setup.sh $1
